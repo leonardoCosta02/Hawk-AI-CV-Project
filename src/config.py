a1057f65e65f5744ca9a951ea52c3a2925158eb0
leonardoCosta02 <costantini.2009905@studenti.uniroma1.it>
2025-12-10 20:18:50 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ HOUGH_COMMON_PARAMS = {
     'THETA': np.pi / 180,   
     'MIN_LENGTH': 60,       # OTTIMIZZATO: Aumentato da 60 a 80 per ridurre il rumore.
     'MAX_GAP': 15,          
-    'ANGLE_TOLERANCE_DEG': 10,
+    'ANGLE_TOLERANCE_DEG': 15,
 }
 
 # --- LISTA DEI PERCORSI DEI FRAME PER IL CARICAMENTO ---
