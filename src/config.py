d498576d381b6519d31a7dcdc11d75fe6bb9709a
leonardoCosta02 <costantini.2009905@studenti.uniroma1.it>
2025-12-10 18:17:02 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -17,13 +17,7 @@ CAMPI_PATH = {
     "TERRA_BATTUTA": 'data/static_court/static_court_frame_clay.png',
 }
 
-# --- PARAMETRI DI CROPPING FISSI (M1) (Forniti dall'utente) ---
-# Queste coordinate (x_start, y_start, x_end, y_end) isolano il campo da tennis
-CROPPING_PARAMS = {
-    'CEMENTO': [70, 150, 1100, 1100],    # Ritaglio per CEMENTO
-    'ERBA': [70, 150, 1100, 900],        # Ritaglio per ERBA
-    'TERRA_BATTUTA': [150, 100, 900, 550], # Ritaglio per TERRA_BATTUTA
-}
+
 
 
 # --- PARAMETRI OTTIMALI CANNY (M1) PER SUPERFICIE ---
